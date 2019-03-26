8eb9a558fab6d68d86bae581362da33db522842d
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2019-03-26 21:02:20 +0100

    init commit with bable fix

---
+++ README.md
@@ -0,0 +1,6 @@
+# Adeva Frontend Task
+
+## Getting Started
+
+* Run `yarn && yarn start` to install all dependencies and start the application.
+* Go to [home](http://localhost:8081) to get started.
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import {render} from "react-dom"
+
+const App = () => (<h1>fkvdjfvkdjfkdj</h1>)
+
+const rootElem = document.getElementById('root');
+
+render(
+	<App />,
+	rootElem
+)
+
+// const html = rootElem.innerHTML;
+// rootElem.innerHTML = 'Welcome to the Adeva take home test. Please follow the instructions in the README and have fun 😃.';
