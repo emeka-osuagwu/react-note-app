25f83259d89029ee085c034416a940aa7bfb27e3
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2019-03-31 17:15:20 +0100

    refactor navbar

--- src/components/common/navbar.js
+++ src/components/common/navbar.js
@@ -1,25 +1,18 @@
 import React from 'react';
 
-export default class SideNav extends React.Component {
-	
-	constructor(props) {
-		super(props);
-	}
+export default function NavBar(props){
+	return (
+		<nav className="navbar navbar-light navbar-dark bg-dark">
+			<a className="navbar-brand" href="#">
+				<img className="d-inline-block align-top" src="images/logo.png" style={{width: 30, height: 30}} />
+				NoteApp
+			</a>
 
-	render() {
-		return (
-			<nav className="navbar navbar-light navbar-dark bg-dark">
-				<a className="navbar-brand" href="#">
-					<img className="d-inline-block align-top" src="images/logo.png" style={{width: 30, height: 30}} />
-					NoteApp
-				</a>
-
-				<ul className="nav justify-content-end">
-				  <li className="nav-item">
-				    <a className="nav-link active btn btn-primary" href="#" onClick={this.props.fireHandleShow}>Create</a>
-				  </li>
-				</ul>
-			</nav>
-		)
-	}
+			<ul className="nav justify-content-end">
+			  <li className="nav-item">
+			    <a className="nav-link active btn btn-primary" href="#" onClick={props.fireHandleShow}>Create</a>
+			  </li>
+			</ul>
+		</nav>
+	)
 }
